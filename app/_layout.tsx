96325aeb659f5661e4a845c66ae9436ad715fa3b
san-sae <choi.yoo.lim.12@gmail.com>
2026-02-28 22:22:29 +0900

    chore: 앱 기본 배경 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,17 @@
 import { Stack } from "expo-router";
 
+import { colors } from "@/src/constants";
+
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        contentStyle: {
+          flex: 1,
+          backgroundColor: colors.bgSecondary,
+        },
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Home" }} />
     </Stack>
   );
