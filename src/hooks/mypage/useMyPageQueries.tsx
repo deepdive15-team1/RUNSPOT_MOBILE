ca32f3db54add19afa777f8ab47baa1a014ab581
Minho <mhgeum07@gmail.com>
2026-06-23 14:52:13 +0900

    fix: 전체 새로고침 스피너 오작동 수정 및 개별 로딩 상태(isFetching) 적용

--- src/hooks/mypage/useMyPageQueries.tsx
+++ src/hooks/mypage/useMyPageQueries.tsx
@@ -30,6 +30,16 @@ export function useMyPageQueries() {
         refetchCreatedRuns: results[1].refetch,
         refetchAppliedRuns: results[2].refetch,
         refetchHistoryRuns: results[3].refetch,
+
+        isProfileError: results[0].isError,
+        isCreatedRunsError: results[1].isError,
+        isAppliedRunsError: results[2].isError,
+        isHistoryRunsError: results[3].isError,
+
+        isProfileFetching: results[0].isFetching,
+        isCreatedRunsFetching: results[1].isFetching,
+        isAppliedRunsFetching: results[2].isFetching,
+        isHistoryRunsFetching: results[3].isFetching,
       };
     },
   });
