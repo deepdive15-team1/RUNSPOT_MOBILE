67efb7d12f2d3922c2f129b3f3ca8165524fb1aa
Minho <mhgeum07@gmail.com>
2026-08-19 15:38:46 +0900

    feat: Firebase Analytics 초기 환경 세팅 및 유틸리티 구축

---
+++ src/utils/analytics.ts
@@ -0,0 +1,27 @@
+import {
+  getAnalytics,
+  logEvent,
+  setUserId,
+} from "@react-native-firebase/analytics";
+
+type EventParams = Record<string, string | number | boolean | null | undefined>;
+
+const analytics = getAnalytics();
+
+export const AnalyticsHelper = {
+  setUserId: async (userId: string | null) => {
+    try {
+      await setUserId(analytics, userId);
+    } catch (err) {
+      console.error("Analytics error: ", err);
+    }
+  },
+
+  logEvent: async (eventName: string, params: EventParams) => {
+    try {
+      await logEvent(analytics, eventName, params);
+    } catch (err) {
+      console.error("Analytics error:", err);
+    }
+  },
+};
